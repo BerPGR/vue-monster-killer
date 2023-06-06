5e818cd8bc28898c99d6ec0419f8a171a5b374b5
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-06-06 19:32:47 -0300

    Insert first content in a div and made a custom design

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 /* eslint-disable */
 <template>
   <div id="app">
-    <Title />
-    <div class="lifes">
-      <Life />
-      <Enemy />
-    </div>
-    <div class="buttons">
-      <Selection bgColor="red" buttonName="Attack" :playerAction="attack"/>
-      <Selection bgColor="yellow" buttonName="Defend" :playerAction="defend"/>
-      <Selection bgColor="green" buttonName="Cure" :playerAction="cure"/>
-      <Selection bgColor="aqua" buttonName="Special" :playerAction="special"/>
+    <div class="first">
+      <Title />
+      <div class="lifes">
+        <Life />
+        <Enemy />
+      </div>
+      <div class="buttons">
+        <Selection bgColor="red" buttonName="Attack" :playerAction="attack"/>
+        <Selection bgColor="yellow" buttonName="Defend" :playerAction="defend"/>
+        <Selection bgColor="green" buttonName="Cure" :playerAction="cure"/>
+        <Selection bgColor="aqua" buttonName="Special" :playerAction="special"/>
+      </div>
     </div>
   </div>
 </template>
@@ -32,7 +34,7 @@ export default {
   },
   methods: {
     attack() {
-      const playerAttack = Math.floor(Math.random() * 15) + 5
+      const playerAttack = Math.floor(Math.random() * 10) + 5
       this.$store.commit('setAttackMovement', playerAttack)
     },
     defend() {
@@ -66,8 +68,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+  background-color: #999;
+  height: 100vh;
+}
+
+.first {
+  margin-top: 20px;
+  background-color: #fff;
+  max-width: 1400px;
+  width: 100%;
+  padding-bottom: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 20px;
+  box-shadow: 2px 15px 25px #555
 }
 
 .lifes {
